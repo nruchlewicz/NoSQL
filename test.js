b36212a82036bfb6ba4a741641abb60ec82223f9
nruchlewicz <n.ruchlewicz@wp.pl>
2016-01-17 11:29:19 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,64 @@
+```js
+$(function () {
+
+    // Radialize the colors
+    Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function (color) {
+        return {
+            radialGradient: {
+                cx: 0.5,
+                cy: 0.3,
+                r: 0.7
+            },
+            stops: [
+                [0, color],
+                [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+            ]
+        };
+    });
+
+    // Build the chart
+    $('#container').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Najczęściej występujące nazwy restauracji'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    },
+                    connectorColor: 'silver'
+                }
+            }
+        },
+        series: [{
+            name: 'Brands',
+            data: [
+                { name: 'Curry', y: 902 },
+                {
+                    name: 'Pizza',
+                    y: 500,
+                    sliced: true,
+                    selected: true
+                },
+                { name: 'Chinese', y: 174 },
+                { name: 'Belati hut', y: 18 }, { name: 'BBQ Express', y: 15},
+                { name: 'inne', y: 972 }
+            ]
+        }]
+    });
+});
+```
